4f3e3558a20ed7cd49bca3965fe50cda20a0a6d3
Alan Da Costa <alandacosta@gmail.com>
2010-05-13 00:39:13 -0700

    Remove timestamps from DataMigration to match AR style

--- lib/mongoid_rails_migrations.rb
+++ lib/mongoid_rails_migrations.rb
@@ -1,13 +1,5 @@
 # encoding: utf-8
 
-# Add base to path
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-# Add rails generators to path
-$:.unshift(File.dirname(__FILE__) + '/rails') unless
-  $:.include?(File.dirname(__FILE__)  + '/rails') || $:.include?(File.expand_path(File.dirname(__FILE__)  + '/rails'))
-
 require 'rubygems'
 # Set up gems listed in the Gemfile.
 if File.exist?(File.expand_path('../../Gemfile', __FILE__))
@@ -17,10 +9,9 @@ end
 
 Bundler.require(:default) if defined?(Bundler)
 
-# require 'rails'
-# require 'railties'
-# require 'activesupport'
-# require 'mongoid'
+# Add base to path incase not included as a gem
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'mongoid_rails_migrations/models/data_migration'
 require 'mongoid_rails_migrations/mongoid_ext/mongoid'
