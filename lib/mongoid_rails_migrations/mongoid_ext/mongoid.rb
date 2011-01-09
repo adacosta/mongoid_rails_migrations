4b0f82273b386779c57ff7d0e51433bf7599b035
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-01-08 16:11:21 -0800

    work with latest mongoid 2.0.0.rc.1

--- lib/mongoid_rails_migrations/mongoid_ext/mongoid.rb
+++ lib/mongoid_rails_migrations/mongoid_ext/mongoid.rb
@@ -1,13 +1,6 @@
 # encoding: utf-8
 
-module Mongoid #:nodoc
-  class Config
-    # Specify whether or not to use timestamps for migration versions
-    cattr_accessor :timestamped_migrations
-    @@timestamped_migrations ||= true # default true
-
-    def self.reset
-      @@timestamped_migrations = true
-    end
-  end
+module Mongoid::Config
+  # Specify whether or not to use timestamps for migration versions
+  option :timestamped_migrations, :default => true
 end
