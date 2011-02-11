914a473f49364c2d632317991a2deffb711b3dc5
Alan Da Costa <alandacosta@gmail.com>
2011-02-10 20:37:35 -0800

    Support pre/post Mongoid::Config 2.0.0.rc.1 class/module

--- lib/mongoid_rails_migrations/mongoid_ext/mongoid.rb
+++ lib/mongoid_rails_migrations/mongoid_ext/mongoid.rb
@@ -1,6 +1,23 @@
 # encoding: utf-8
-
-module Mongoid::Config
+module Mongoid
   # Specify whether or not to use timestamps for migration versions
-  option :timestamped_migrations, :default => true
+  # NOTE: newer style is a module
+  # Config.module_eval would work for both but still need to determine the type
+  # that's why we do it the ug way.
+  if Config.is_a? Class
+    # older mongoid style; pre 2.0.0.rc.1
+    Config.module_eval do
+      cattr_accessor :timestamped_migrations
+      @@timestamped_migrations ||= true # default true
+
+      def self.reset
+        @@timestamped_migrations = true
+      end
+    end
+  else # module
+    Config.module_eval do
+      # newer mongoid style; >= 2.0.0.rc.1
+      option :timestamped_migrations, :default => true
+    end
+  end
 end
