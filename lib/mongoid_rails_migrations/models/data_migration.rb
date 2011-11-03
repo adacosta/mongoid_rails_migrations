8a25dfecffb0f868cc14851f3a9f86c13ddff876
ara.t.howard <ara.t.howard@gmail.com>
2011-11-03 15:42:07 -0600

    life is hard across mongoid versions with non object_id ids.  use strings

--- lib/mongoid_rails_migrations/models/data_migration.rb
+++ lib/mongoid_rails_migrations/models/data_migration.rb
@@ -1,5 +1,22 @@
 class DataMigration
   include Mongoid::Document
 
+##
+# life gets harder when the id isn't a string...
+
+  field(:_id, :type => String, :default => proc{ App.uuid })
+
+  if respond_to?(:identity)
+    begin
+      identity(:type => String, :default => proc{ App.uuid })
+    rescue
+      nil
+    end
+  end
+
+  if respond_to?(:using_object_ids)
+    self.using_object_ids = false
+  end
+
   field :version
 end
