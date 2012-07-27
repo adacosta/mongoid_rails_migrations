30a4b477e2290666a5baa3c8c7dc41183b2f5425
Andrew Chase <acechase@andrew-chases-computer-2.local>
2012-07-27 09:21:50 -0700

    use Mongoid.configure rather than Mongoid.config. Mongoid.config is an alias of configure and has been removed in mongoid 3.

--- lib/rails/generators/mongoid/mongoid_generator.rb
+++ lib/rails/generators/mongoid/mongoid_generator.rb
@@ -15,7 +15,7 @@ module Mongoid #:nodoc:
       # Implement the required interface for Rails::Generators::Migration.
       def self.next_migration_number(dirname) #:nodoc:
         next_migration_number = current_migration_number(dirname) + 1
-        if Mongoid.config.timestamped_migrations
+        if Mongoid.configure.timestamped_migrations
           [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
         else
           "%.3d" % next_migration_number
