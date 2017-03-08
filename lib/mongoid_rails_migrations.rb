d2d197ca585324cfd09ad7d52be47de221856ccd
Martin Moen Wulffeld <martin@wulffeld.org>
2017-03-08 19:33:56 +0100

    Remove unnecessary loading of gems from Gemfile.

--- lib/mongoid_rails_migrations.rb
+++ lib/mongoid_rails_migrations.rb
@@ -1,10 +1,6 @@
 # encoding: utf-8
-require 'bundler/setup'
-Bundler.require(:mongoid_rails_migrations)
 
-# Add base to path incase not included as a gem
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+require 'mongoid'
 
 require 'mongoid_rails_migrations/version'
 require 'mongoid_rails_migrations/models/data_migration'
