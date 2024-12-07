d9ed529c04eba45a6dfa8b3000139079f112f908
Heitor Danilo <heitor.danilo@icloud.com>
2024-12-07 18:57:06 -0300

    feat!: prefix Rake tasks with the db:mongoid namespace
    Updated all Rake tasks to use the `db:mongoid:` namespace instead of
    `db:`.
    
    For compatibility, tasks (e.g., `db:migrate:create`) are aliased to
    their `db:mongoid:` counterparts (e.g., `db:migrate:create` ->
    `db:mongoid:migrate:create`) if not already defined. These aliased tasks
    are now marked as deprecated.
    
    The README has been updated to recommend using the new `db:mongoid:`
    namespace as the preferred approach.

--- test/tasks/version_task_test.rb
+++ test/tasks/version_task_test.rb
@@ -4,32 +4,32 @@ module Mongoid
   class VersionTaskTest < TaskTestBase
     def test_database_version
       Mongoid::Migrator.migrations_path = [MIGRATIONS_ROOT + "/valid"]
-      assert_output("0\n") { invoke("db:version") }
-      invoke("db:migrate")
-      assert_output("20100513063902\n") { invoke("db:version") }
-      invoke("db:drop")
-      assert_output("0\n") { invoke("db:version") }
+      assert_output("0\n") { invoke("db:mongoid:version") }
+      invoke("db:mongoid:migrate")
+      assert_output("20100513063902\n") { invoke("db:mongoid:version") }
+      invoke("db:mongoid:drop")
+      assert_output("0\n") { invoke("db:mongoid:version") }
     end
 
     def test_multidatabase_version
       Mongoid::Migrator.migrations_path = [MIGRATIONS_ROOT + "/multi_shards"]
-      assert_output("0\n") { invoke("db:version") }
-      invoke("db:migrate")
-      assert_output("20210210125800\n") { invoke("db:version") }
-      invoke("db:drop")
-      assert_output("0\n") { invoke("db:version") }
+      assert_output("0\n") { invoke("db:mongoid:version") }
+      invoke("db:mongoid:migrate")
+      assert_output("20210210125800\n") { invoke("db:mongoid:version") }
+      invoke("db:mongoid:drop")
+      assert_output("0\n") { invoke("db:mongoid:version") }
     end
 
     def test_multidatabase_version_with_target_client_and_rollback
       Mongoid::Migrator.migrations_path = [MIGRATIONS_ROOT + "/multi_shards"]
       with_env("MONGOID_CLIENT_NAME" => "shard1") do
-        assert_output("0\n") { invoke("db:version") }
-        invoke("db:migrate")
-        assert_output("20210210125532\n") { invoke("db:version") }
-        invoke("db:rollback")
-        assert_output("20210210124656\n") { invoke("db:version") }
-        invoke("db:rollback")
-        assert_output("0\n") { invoke("db:version") }
+        assert_output("0\n") { invoke("db:mongoid:version") }
+        invoke("db:mongoid:migrate")
+        assert_output("20210210125532\n") { invoke("db:mongoid:version") }
+        invoke("db:mongoid:rollback")
+        assert_output("20210210124656\n") { invoke("db:mongoid:version") }
+        invoke("db:mongoid:rollback")
+        assert_output("0\n") { invoke("db:mongoid:version") }
       end
     end
   end
